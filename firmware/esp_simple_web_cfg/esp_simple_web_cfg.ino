eb3d12dbbf6a116669274134f1cfe72aaa16f277
lmtreser <lmtreser@gmail.com>
2024-01-29 11:22:22 -0300

    update ino

--- firmware/esp_simple_web_cfg/esp_simple_web_cfg.ino
+++ firmware/esp_simple_web_cfg/esp_simple_web_cfg.ino
@@ -1,42 +1,56 @@
 /*
     Simple Portal Web para ESP8266
     Permite configurar fácilmente el acceso a una red WiFi a un dispositivo IoT
+    La IP por defecto para conectarse es 192.168.4.1
 */
 
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
+#include "espSetup.h"
 #include "html.h"
 
 const char* wifiSsid = "";
 const char* wifiPassword = "";
-const int BOTON = 5;  // GPIO5 = D1
-bool buttonState;
 
-ESP8266WebServer server{ 80 };
+const int MAX_ATTEMPTS = 50;
+const int RESET = 5;  // GPIO5 = D1
 
-void handleRoot() {
+ESP8266WebServer server(80);
 
-  // Si llegan datos desde el formulario, ejecutar
-  if (!(server.arg("ssid").isEmpty()) || !(server.arg("password").isEmpty())) {
+// Conectar a la red WiFi
+void wifiConnect() {
 
-    wifiSsid = server.arg("ssid").c_str();
-    wifiPassword = server.arg("password").c_str();
-    Serial.println(wifiSsid);
-    Serial.println(wifiPassword);
-    server.send(200, "text/html", htmlSuccess);
+  int attempts = 0;
 
-    delay(1000);
-    WiFi.softAPdisconnect(true);  // Desconectar el modo AP
-    wifiConnect();                // Conectar a WiFi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifiSsid, wifiPassword);
+
+  Serial.println("Ejecutando en modo STA");
+  Serial.print("Conectado con el SSID: " + WiFi.SSID());
+  Serial.println(" y Password: " + WiFi.psk());
+
+  // Intentar conectar a WiFi
+  while ((WiFi.status() != WL_CONNECTED) && (attempts < MAX_ATTEMPTS)) {
+    delay(100);
+    Serial.print(".");
+    attempts++;
   }
-  server.send(200, "text/html", htmlConfig);
+
+  // Si no se logro la conexion, pasar al modo AP y lanzar el portal
+  if (WiFi.status() != WL_CONNECTED) {
+    wifiAP();
+  } else {
+    Serial.print("\nConexión exitosa a la red WiFi con IP: ");
+    Serial.println(WiFi.localIP());
+  }
 }
 
 // Funcionar en modo AP
 void wifiAP() {
 
-  WiFi.mode(WIFI_AP);
   Serial.println("Ejecutando en modo AP");
+
+  WiFi.mode(WIFI_AP);
   while (!WiFi.softAP("ESP8266 WebServer")) {
     Serial.print('.');
     delay(100);
@@ -44,9 +58,7 @@ void wifiAP() {
 
   // WebServer atiende peticiones en '/'
   server.on("/", handleRoot);
-  server.onNotFound([] {
-    server.send(404, "text/plain", "Error 404");
-  });
+  server.onNotFound(handleRoot);
   server.begin();
 
   // Server ok
@@ -54,49 +66,38 @@ void wifiAP() {
   Serial.println(WiFi.softAPIP());
 }
 
-// Conectar a la red WiFi
-void wifiConnect() {
+void handleRoot() {
 
-  WiFi.mode(WIFI_STA);
-  Serial.println("Ejecutando en modo STA");
+  server.sendHeader("Location", "/", true);  // Redirigir al Captive Portal
+  server.send(200, "text/html", htmlConfig); // Enviar el Captive Portal
 
-  if (buttonState) {
-    Serial.print("Conectado con el SSID: " + WiFi.SSID());
-    Serial.println(" y Password: " + WiFi.psk());
-    WiFi.begin();
-  } else {
-    WiFi.begin(wifiSsid, wifiPassword);
+  // Si llegan datos desde el formulario, ejecutar
+  if (!(server.arg("ssid").isEmpty()) || !(server.arg("password").isEmpty())) {
+
+    wifiSsid = server.arg("ssid").c_str();
+    wifiPassword = server.arg("password").c_str();
+    server.send(200, "text/html", htmlSuccess);
+
+    //delay(1000);
+    WiFi.softAPdisconnect(true);  // Desconectar el modo AP
+    wifiConnect();                // Conectar a WiFi
   }
-
-  Serial.print("Conectando a WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
-    Serial.print(".");
-  }
-  Serial.print("\nConexión exitosa a la red WiFi con IP: ");
-  Serial.println(WiFi.localIP());
 }
 
 void setup() {
 
-  pinMode(BOTON, INPUT_PULLUP);
   Serial.begin(9600);
+  pinMode(RESET, INPUT_PULLUP);
 
-  buttonState = digitalRead(BOTON);
-
-  // De acuerdo al estado del botón de configuración hacer...
-  if (buttonState) {
-    wifiConnect();
-  } else {
-    wifiAP();
-  }
+  // Conectar a WiFi
+  wifiConnect();
 }
 
 void loop() {
 
-  if (buttonState) {
-    // Acá tu código una vez establecida la conexión
-  } else {
-    server.handleClient();  // Ejecución en modo AP
-  }
+  server.handleClient();  // Ejecución en modo AP
+
+  // Resetear las credenciales almacenadas
+  bool buttonState = digitalRead(RESET);
+  if (buttonState == LOW) WiFi.disconnect(true);
 }
